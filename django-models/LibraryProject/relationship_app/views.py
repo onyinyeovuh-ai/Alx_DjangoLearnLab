ff14485608c8a0de0232056b8a7bc460f600dd90
Onyinye Ovuh <onyinyeovuh@gmail.com>
2026-01-18 14:12:09 +0100

    Update list_books view to render relationship_app/list_books.html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
