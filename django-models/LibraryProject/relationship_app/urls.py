b9911a695ebdd06cdae045f9ed6d10afc4330b66
Onyinye Ovuh <onyinyeovuh@gmail.com>
2026-01-18 17:55:52 +0100

    Add user authentication with login logout and registration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import (
+    list_books, 
+    LibraryDetailView,  
+    register,
+    UserLoginView,
+    UserLogoutView,
+)
+
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+
+
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
+    path("register/", register, name="register"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,3 +15,30 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect, render
+
+
+# User registration view
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+
+# User login view (class-based)
+class UserLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+
+# User logout view (class-based)
+class UserLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
+
