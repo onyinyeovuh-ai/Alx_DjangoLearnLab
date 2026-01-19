d03bf44fee6c8f3e8c4724fda6b4b7b0de57763c
Onyinye Ovuh <onyinyeovuh@gmail.com>
2026-01-19 02:20:59 +0100

    Fix book URLs to match checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,15 @@
+
 from django.urls import path
-from .views import admin_view, librarian_view, member_view
+from .views import add_book, edit_book, delete_book, admin_view, librarian_view, member_view
 
 urlpatterns = [
+    # Role-based views
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
-]
 
-from .views import add_book, edit_book, delete_book
-
-urlpatterns += [
-    path('book/add/', add_book, name='add_book'),
-    path('book/<int:pk>/edit/', edit_book, name='edit_book'),
-    path('book/<int:pk>/delete/', delete_book, name='delete_book'),
+    # Book views with permissions
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
 ]
